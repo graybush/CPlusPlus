7ab2ef19e03dcda3bfca1623f80a066834d9f3c9
graybush <bpbeatty@gmail.com>
2016-11-24 23:57:47 -0600

    correct Euler ans obtained

--- Project Euler/001_Multiples of 3 and 5/001_Multiples_of_3_and_5.cpp
+++ Project Euler/001_Multiples of 3 and 5/001_Multiples_of_3_and_5.cpp
@@ -5,9 +5,26 @@
 
 using namespace std;
 
+/*
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
 int main(){
 
-cout << "Hello world again" << endl;
+int X;
+int Y;
+int Z;
+
+cout << "I will calculate the Multiples of X and Y until Z: " << endl;
+cout << "X? ";
+cin >> X;
+cout << "Y? ";
+cin >> Y;
+cout << "Z? ";
+cin >> Z;
+cout << X, Y, Z;
 
 
 return 0;
