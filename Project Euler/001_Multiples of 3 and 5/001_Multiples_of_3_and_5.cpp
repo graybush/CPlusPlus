d800a545510e4091457097cf7281471afae375f3
centos <bpbeatty@gmail.com>
2018-08-31 23:30:41 -0500

    Removes trailing whitespace

--- Project Euler/001_Multiples of 3 and 5/001_Multiples_of_3_and_5.cpp
+++ Project Euler/001_Multiples of 3 and 5/001_Multiples_of_3_and_5.cpp
@@ -6,7 +6,8 @@
 using namespace std;
 
 /*
-If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
+3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
@@ -19,7 +20,8 @@ int Z;
 int total;
 
 
-cout << "I will calculate the sum of all the multiples of  X and Y below Z: " << endl;
+cout << "I will calculate the sum of all the multiples of  X and Y below Z: "
+  << endl;
 cin >> X;
 cin >> Y;
 cin >> Z;
@@ -34,7 +36,8 @@ cin >> Z;
 
   } 
 
-cout << "The sum of all the multiples of " << X << " and " << Y << " below " << Z << " are: " << total << endl;
+cout << "The sum of all the multiples of " << X << " and " << Y << " below " 
+  << Z << " are: " << total << endl;
 
 
 return 0;
