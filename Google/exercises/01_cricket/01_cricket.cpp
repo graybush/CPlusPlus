3a2f20409e7a070190c5915532b6014c045c21df
centos <bpbeatty@gmail.com>
2018-08-31 23:25:02 -0500

    Removes trailing whitespace

--- Google/exercises/01_cricket/01_cricket.cpp
+++ Google/exercises/01_cricket/01_cricket.cpp
@@ -3,41 +3,31 @@
 
 using namespace std;
 
-float chirps2temp(int chirps);
+float chirps2temp(const int &chirps);
 
+// https://developers.google.com/edu/c++/getting-started
 int main (){
 
   int input_var = 0;
   bool valid = false;
 
   do {
-
 	  cout << "Number of chirps: (-1 = quit) " << endl;
 	  cin >> input_var;
-
 	  while (!(cin >> input_var)){
-
 		cout << "A non-numeric was entered." << endl;
 		cin.clear();
 		cin.ignore(10000, '\n');
-
 	  }
-
 	  cout << "The temperature is: " << chirps2temp(input_var) << endl;
-
     } while (input_var != -1);
-    
     cout << "Done." << endl;
-
   return 0;
 }
 
 
-float chirps2temp(int chirps){
-
+float chirps2temp(const int &chirps) {
   float temp;
-
   temp = (chirps + 40) / 4;
-
   return temp;
 }
--- Google/exercises/02_finalGrade/02_finalGrade.cpp
+++ Google/exercises/02_finalGrade/02_finalGrade.cpp
@@ -1,17 +1,16 @@
-#include <iostream>
 #include <cstdlib>
+#include <iostream>
 
 using namespace std;
 
 float get_input(string assignment);
 
-float cal_grade(float first, float second, float third,
-                float fourth, float midterm, float final, float section);
+float cal_grade(float first, float second, float third, float fourth,
+    float midterm, float final, float section);
 
 int main(){
 
   float first, second, third, fourth, midterm, final, section, grade;
-
   first = get_input("first");
   second = get_input("second");
   third = get_input("third");
@@ -19,52 +18,36 @@ int main(){
   midterm = get_input("midterm");
   final = get_input("final");
   section = get_input("section");
-
   grade = cal_grade(first, second, third, fourth, midterm, final, section);
-
   cout << "The final grade is: " << grade << endl;
-
-
-
   return 0;
 }
 
 float get_input(string num){
 
   float value = 0.0;
-
   do {
-    cout << "Enter the score for the " << num << " assignment. (-1 = quit) " << endl;
-
+    cout << "Enter the score for the " << num << " assignment. (-1 = quit) " <<
+      endl;
     if (value == 0){
-
-    	cout << "Done.\n";
-    	break;
-
+      cout << "Done.\n";
+      break;
     } else if (!(cin >> value)){
-
       cout << "A non-numeric was entered." << endl;
       cin.clear();
       cin.ignore(10000, '\n');
-
     } else {
-
       return value;
     }
   } while (value != -1);
-
-    cout << "Done." << endl;
-
+  cout << "Done." << endl;
 }
 
-float cal_grade(float first, float second, float third,
-                float fourth, float midterm, float final, float section){
-
+float cal_grade(float first, float second, float third, float fourth,
+    float midterm, float final, float section) {
   float grade;
-
-  grade = (((first + second + third + fourth) / 4) * 0.4) 
-          + (midterm * 0.15) + (final * 0.35) + (section * 0.1);
-
+  grade = (((first + second + third + fourth) / 4) * 0.4)
+    + (midterm * 0.15) + (final * 0.35) + (section * 0.1);
   return grade;
 }
 
